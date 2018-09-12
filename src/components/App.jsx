a92588ae9dc0a8db8f256f989d6d1d8e298e52f8
Nick Rogers and Swati Sahay <swati@>
2018-09-12 13:59:39 -0700

    Recursive sorting works.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,6 +17,7 @@ class App extends React.Component {
     this.handleAddingDownvotes = this.handleAddingDownvotes.bind(this);
     this.handleSortingPosts = this.handleSortingPosts.bind(this);
     this.handleMergingArrays = this.handleMergingArrays.bind(this);
+    this.handleAutoSorting = this.handleAutoSorting.bind(this);
   }
   handleAddingNewPostToList(newPost){
     var newMasterPostList = this.state.masterPostList.slice();
@@ -31,7 +32,6 @@ class App extends React.Component {
       }
     }
     this.setState({masterPostList: newMasterPostList});
-    this.tests();
   }
   handleAddingDownvotes(id){
     var newMasterPostList = this.state.masterPostList.slice();
@@ -46,21 +46,22 @@ class App extends React.Component {
     let n = arrayToSort.length;
     if(n < 2) {
       return;
+    } else {
+      let mid = Math.floor(arrayToSort.length/2);
+      let left = arrayToSort.slice(0, mid);
+      let right = arrayToSort.slice(mid);
+      this.handleSortingPosts(left);
+      this.handleSortingPosts(right);
+      return this.handleMergingArrays(left, right, arrayToSort);
     }
-    let mid = Math.floor(arrayToSort.length/2);
-    let left = arrayToSort.split(0, mid);
-    let right = arrayToSort.split(mid);
-    this.handleSortingPosts(left);
-    this.handleSortingPosts(right);
-    this.handleMergingArrays(left, right, arrayToSort);
   }
   handleMergingArrays(leftArray, rightArray, fullArray){
+    console.log(fullArray);
     let i = 0;
     let j = 0;
     let k = 0;
-
-    while(i < leftArray.length || j < rightArray.length){
-      if(leftArray[i].upvotes <= rightArray[j].upvotes){
+    while(i < leftArray.length && j < rightArray.length){
+      if(leftArray[i].upvotes >= rightArray[j].upvotes){
         fullArray.splice(k,1,leftArray[i]);
         i++;
       } else {
@@ -76,10 +77,16 @@ class App extends React.Component {
     }
     while(j < rightArray.length){
       fullArray.splice(k,1,rightArray[j]);
+      j++;
+      k++;
     }
+    console.log(fullArray);
+    return fullArray;
   }
   handleAutoSorting(){
-    
+    var newMasterPostList = this.state.masterPostList.slice();
+    let sortedPostList = this.handleSortingPosts(newMasterPostList);
+    this.setState({masterPostList: newMasterPostList});
   }
 
 
@@ -88,7 +95,7 @@ class App extends React.Component {
       <div>
         <Header/>
         <div className="container">
-          <button className="btn btn-info" onClick={this.handleSortingPosts}>Sort</button>
+          <button className="btn btn-info" onClick={this.handleAutoSorting}>Sort</button>
           <Switch>
             <Route exact path='/' render={()=><PostList postList={this.state.masterPostList}
             onAddingUpvotes={this.handleAddingUpvotes}
