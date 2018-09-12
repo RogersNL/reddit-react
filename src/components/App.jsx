2ada42ce152896de6362a01c321d32805e3681e8
Nick Rogers and Swati Sahay <swati@>
2018-09-12 11:38:22 -0700

    Add sorting method (inc)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,6 +13,10 @@ class App extends React.Component {
       masterPostList: []
     };
     this.handleAddingNewPostToList = this.handleAddingNewPostToList.bind(this);
+    this.handleAddingUpvotes = this.handleAddingUpvotes.bind(this);
+    this.handleAddingDownvotes = this.handleAddingDownvotes.bind(this);
+    this.handleSortingPosts = this.handleSortingPosts.bind(this);
+    this.handleMergingArrays = this.handleMergingArrays.bind(this);
   }
   handleAddingNewPostToList(newPost){
     var newMasterPostList = this.state.masterPostList.slice();
@@ -20,24 +24,71 @@ class App extends React.Component {
     this.setState({masterPostList: newMasterPostList});
   }
   handleAddingUpvotes(id){
-    let currentPost = null;
-    console.log(this.state.masterPostList);
-    // for(let i = 0; i < this.state.masterPostList.length; i++){
-    //   if(this.state.masterPostList[i].id === id){
-    //     currentPost = clickedPost;
-    //   }
-    // }
-    // console.log(currentPost);
+    var newMasterPostList = this.state.masterPostList.slice();
+    for(let i = 0; i < newMasterPostList.length; i++){
+      if(newMasterPostList[i].id === id){
+        newMasterPostList[i].upvotes = newMasterPostList[i].upvotes + 1;
+      }
+    }
+    this.setState({masterPostList: newMasterPostList});
+    this.tests();
   }
   handleAddingDownvotes(id){
-    alert(id);
+    var newMasterPostList = this.state.masterPostList.slice();
+    for(let i = 0; i < newMasterPostList.length; i++){
+      if(newMasterPostList[i].id === id){
+        newMasterPostList[i].downvotes = newMasterPostList[i].downvotes + 1;
+      }
+    }
+    this.setState({masterPostList: newMasterPostList});
   }
+  handleSortingPosts(arrayToSort){
+    let n = arrayToSort.length;
+    if(n < 2) {
+      return;
+    }
+    let mid = Math.floor(arrayToSort.length/2);
+    let left = arrayToSort.split(0, mid);
+    let right = arrayToSort.split(mid);
+    this.handleSortingPosts(left);
+    this.handleSortingPosts(right);
+    this.handleMergingArrays(left, right, arrayToSort);
+  }
+  handleMergingArrays(leftArray, rightArray, fullArray){
+    let i = 0;
+    let j = 0;
+    let k = 0;
+
+    while(i < leftArray.length || j < rightArray.length){
+      if(leftArray[i].upvotes <= rightArray[j].upvotes){
+        fullArray.splice(k,1,leftArray[i]);
+        i++;
+      } else {
+        fullArray.splice(k,1,rightArray[j]);
+        j++;
+      }
+      k++;
+    }
+    while(i < leftArray.length){
+      fullArray.splice(k,1,leftArray[i]);
+      i++;
+      k++;
+    }
+    while(j < rightArray.length){
+      fullArray.splice(k,1,rightArray[j]);
+    }
+  }
+  handleAutoSorting(){
+    
+  }
+
 
   render(){
     return (
       <div>
         <Header/>
         <div className="container">
+          <button className="btn btn-info" onClick={this.handleSortingPosts}>Sort</button>
           <Switch>
             <Route exact path='/' render={()=><PostList postList={this.state.masterPostList}
             onAddingUpvotes={this.handleAddingUpvotes}
